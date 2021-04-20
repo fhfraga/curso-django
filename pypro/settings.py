d106079a2d36f7bfd024bece8e2d67cfa6bbfc98
Fernando <f.henrique.fraga@gmail.com>
2021-04-20 18:20:08 -0300

    ajustado settings.py

--- pypro/settings.py
+++ pypro/settings.py
@@ -63,7 +63,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'collectfast'
+    'collectfast',
     'django.contrib.staticfiles',
     'pypro.base.apps',
 ]
