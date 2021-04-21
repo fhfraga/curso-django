881c280ee7481bb74613309dda1fe363d3b84e5f
Fernando <f.henrique.fraga@gmail.com>
2021-04-21 09:01:04 -0300

    adicao da app base em INSTALLED_APPS

--- pypro/settings.py
+++ pypro/settings.py
@@ -64,7 +64,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base'
+    'pypro.base'
 ]
 
 # comando para informar ao django a classe base utilizada como usuário
