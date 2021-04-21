c6be6cceabef8456d2384541dab0e0bbfaf2aa06
Fernando <f.henrique.fraga@gmail.com>
2021-04-21 09:01:36 -0300

    adicao da url de base para a url do site

--- pypro/urls.py
+++ pypro/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import base
+#import base
 
 # fazer mapeamento do que está na view
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base.urls')),
+    path('', include('pypro.base.urls')),
 ]
