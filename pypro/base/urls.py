8adc79776e20e401e8ef851dbceea777a0ed1458
Fernando <f.henrique.fraga@gmail.com>
2021-04-23 16:59:57 -0300

    modificacao para urls de base ficarem explicitas no html

--- pypro/base/urls.py
+++ pypro/base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'base' # serve para emcapsular melhor as urls para saber de qual app é
 urlpatterns =[
     path('', views.home, name='home')
 ]
