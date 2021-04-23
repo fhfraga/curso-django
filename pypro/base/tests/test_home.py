da7c0cb709521b8899e45a1ef05ea7fbfc455471
Fernando <f.henrique.fraga@gmail.com>
2021-04-23 16:59:23 -0300

    adicao dos testes para url

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -1,11 +1,24 @@
 from django.test import Client
+from django.urls.base import reverse
 from pypro.django_assertions import assert_contains
+import pytest
 
-def test_status_code(client: Client):
-    resp = client.get('/')
+# reverse serve para poder variar o nome sem precisar ficar alterando o código
+# pois agora o parametro é o nome do path. agora a url é dependente apenas do 
+# nome (name na url)
+@pytest.fixture
+def resp(client):
+    resp = client.get(reverse('base:home'))
+    return resp
+
+
+def test_status_code(resp):
     assert resp.status_code == 200
 
 
-def test_title(client: Client):
-    resp = client.get('/')
-    assert_contains(resp, '<title>Python Pro</title>') 
+def test_title(resp):
+    assert_contains(resp, '<title>Python Pro</title>')
+
+
+def test_home_link(resp):
+    assert_contains(resp, f'href="{reverse("base:home")}">Python Pro</a>')
