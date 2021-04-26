df7cc1fc75d2f80dd219f1a5bbf4fe548c44f8bd
Fernando <f.henrique.fraga@gmail.com>
2021-04-26 16:59:58 -0300

    adicionado url para aplicativo aperitivos

--- pypro/aperitivos/urls.py
+++ pypro/aperitivos/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from pypro.aperitivos.views import video
+
+from pypro.aperitivos.views import video, indice
 
 app_name = 'aperitivos' # serve para emcapsular melhor as urls para saber de qual app é
 urlpatterns =[
-    path('<slug:slug>', video, name='video') # slug é um identificador para os vídeos
+    path('<slug:slug>', video, name='video'), # slug é um identificador para os vídeos
+    path('', indice, name='indice'),
 ]
