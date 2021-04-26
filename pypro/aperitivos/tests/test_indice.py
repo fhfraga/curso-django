e49c68f31e20aa7904e6ffa20823102c772d721c
Fernando <f.henrique.fraga@gmail.com>
2021-04-26 18:22:35 -0300

    adicionado novos testes para o indices

--- pypro/aperitivos/tests/test_indice.py
+++ pypro/aperitivos/tests/test_indice.py
@@ -22,5 +22,13 @@ def test_titulo_video(resp, titulo):
     assert_contains(resp, titulo)
 
 
-# def test_conteudo_video(resp):
-#     assert_contains(resp, '<iframe width="560" height="315" src="https://www.youtube.com/embed/2aYplgJrPDU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+@pytest.mark.parametrize(
+    'slug',
+    [
+        'motivacao',
+        'instalacao-windows'
+    ]
+)
+def test_link_video(resp, slug):
+    video_link = reverse('aperitivos:video', args=(slug,))
+    assert_contains(resp, f'href="{video_link}"')
