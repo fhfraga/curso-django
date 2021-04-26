aed76b5b021d7a1ef2b3525dc10f135d5f00ac95
Fernando <f.henrique.fraga@gmail.com>
2021-04-26 17:00:29 -0300

    criada vizualizaçao para pagina de aperitivos

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 
+class Video:
+    def __init__(self, slug, titulo, youtube_id):
+        self.slug = slug
+        self.titulo = titulo
+        self.youtube_id = youtube_id
+
+videos=[
+       Video('motivacao', 'Video Aperitivo: Motivação', '2aYplgJrPDU'),
+       Video('instalacao-windows', 'Instalação Python no Windows', 'ScmQ4I5Qr5s')
+]
+
+videos_dct = {v.slug: v for v in videos}
+
+
+def indice(request):
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
 
 
 def video(request, slug):
-    videos={
-        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'youtube_id': '2aYplgJrPDU'},
-        'instalacao-windows': {'titulo': 'Instalação Python no Windows', 'youtube_id': 'ScmQ4I5Qr5s'}
-    }
-    video = videos[slug]
+    video = videos_dct[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
